9b773c63a10d1493cc10859e30b2e41df81ffb9c
victrix-ent <victrixenterprise@gmail.com>
2026-09-03 02:20:42 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -24,11 +24,7 @@ const SIGNAL_API_URL = process.env.SIGNAL_API_URL || 'http://localhost:8080';
 
 const app = express();
 app.use(cors());
-app.use(basicAuth({
-    users: { 'admin': process.env.DASHBOARD_PASSWORD || 'fallback_temporary_password' }, 
-    challenge: true,
-    realm: 'Activity Tracker'
-}));
+)
 
 // Serve static React web files out of the client/build directory
 const clientPath = path.join(process.cwd(), 'client', 'build');
